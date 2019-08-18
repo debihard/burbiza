3bc58dc52040ed0de4652be8f4318a68e9dc005c
John <50121345+debihard@users.noreply.github.com>
2019-08-18 13:12:42 +0500

    Update zipo.sh

--- zipo.sh
+++ zipo.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#usage:
+# curl -O https://raw.githubusercontent.com/debihard/burbiza/master/zipo.sh && chmod +x zipo.sh && ./zipo.sh
+
+
+##############################################################################################################
+archivepassword=$(cat /root/archivepass.txt)
 ##############################################################################################################
 
 # Installing Dependencies
@@ -14,6 +20,7 @@ install_dep(){
    spinner
    yum install sudo
    sudo yum install epel-release -y
+   sudo yum -y install p7zip p7zip-plugins
    sudo -y yum install pwgen
    say_done
 }
@@ -117,24 +124,30 @@ curl -O https://github.com/debihard/burbiza/raw/master/a1.zip
 extract_a1(){
 clear
   f_banner
+  
+ ARCHIVEPASS=/root/archivepass.txt
+if [ -f $ARCHIVEPASS ]; then
+  
   echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
   echo -e "\e[93m[+]\e[00m Extract a1"
   echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
   echo ""
   
-
-  echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-  echo -e "\e[93m[+]\e[00m Extract a1 archive. Please Enter Your Password!"
-  echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
-
-  yum -y install p7zip p7zip-plugins
-  echo ""
-  echo -n " Please Enter Your Password: "; read -s archivepassword
-  #cd a1
   7z x a1.7z -p$archivepassword; echo "extract archive OK"
+  
+ else 
+  echo ""
+  echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
+  echo -e "\e[93m[+]\e[00m Extract a1 archive. File with archive password doesn't exist. Please Enter Your Password!"
+  echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
+  echo ""
+  echo -n " Please Enter Your Password: "; read -s archivepassword2
+  #cd a1
+  7z x a1.7z -p$archivepassword2; echo "extract archive OK"
   #rm -f a1.7z; echo "remove archive OK"
   #cd ..
   echo " OK"
+  fi
 say_done
 }
 
